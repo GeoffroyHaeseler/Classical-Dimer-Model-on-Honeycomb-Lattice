247859b88da4495bb36673724c27099349bdddc5
GeoffroyHaeseler <geoffroy.haeseler@gmail.com>
2024-12-16 15:45:59 +0100

    Delete viewer.h

--- viewer.h
+++
@@ -1,7 +0,0 @@
-void view();
-void viewLattice();
-void addLatticeVertices(int ix, int iy);
-void addLatticeFaces(int ix, int iy);
-void viewResult(int k);
-void addArrowsVertices(int k, int ix, int iy, const char* name);
-void addArrowsFaces(int k, int ix, int iy, const char* name);
