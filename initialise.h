00a44e244de3d86eb2adfb2aa42ad5f7093f207d
GeoffroyHaeseler <geoffroy.haeseler@gmail.com>
2024-12-16 15:46:37 +0100

    Delete initialise.h

--- initialise.h
+++
@@ -1,9 +0,0 @@
-void initialise();
-void magFieldInitialisation(double B0, double phi);
-void latticeInitialisation();
-void lattice2Initialisation();
-void lattice3Initialisation();
-void lattice4Initialisation();
-void intialiseZ3SymmetryBreaking();
-void spinPositionIntialisation();
-void probaInitialisation(double T, double B0, double V0, double phi);
